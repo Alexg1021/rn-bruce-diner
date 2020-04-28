d61f44994fed51ed7841c29e5c50dec0a4bb2ef5
Alex Gutierrez <gutierrez.lx@gmail.com>
2020-04-27 19:33:46 -0700

    feature: adding my screens boilerplate to screens folder

---
+++ screens/ViewOrders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ViewOrdersScreen = () => {
+  return (
+    <View>
+      <Text>My View Orders screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ViewOrdersScreen;
